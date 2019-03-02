81e90422fd08322ac950f99fd1960019a2a6bed1
Palm <viktor.palm.vp@gmail.com>
2019-03-02 13:11:36 +0100

    first working model

---
+++ ute.py
@@ -0,0 +1,88 @@
+"""
+***Introduction***
+------------------
+A library for emulating of the universal machine as outlined in
+Alan Turings 1936 paper  "on computable numbers as
+they relate to the einsheidungsproblem".
+"""
+
+"""
+***Definitions and explanations***
+----------------------------------
+*Let the symbols string be the possible symbols that can be printed on the tape.
+*Let the m_config variable be a dictionary that defines the possible states of the machine,
+    -Let the key of each entry be a string wich is the name of the m_config
+    -Let the entry corespnding to each m_config be another dicionary where 
+        each key coresponds to a possible symbol or a special matching string(explained later).
+    -Let the entry corresponding to
+"""
+
+#Todo
+#Add all keyword
+#add not keyword
+#fix move operation
+
+#def parse(program, tape):
+#    return m_configs, symbols
+
+
+def move(current_m_config, m_configs, tape_pointer, tape):
+    scanned_symbol = tape[tape_pointer]
+    
+    if (scanned_symbol == " "):
+        operations = m_configs[current_m_config]["None"]["operations"]
+        final_m_config = m_configs[current_m_config]["None"]["final-m"]
+    else:
+        operations = m_configs[current_m_config][scanned_symbol]["operations"]
+        final_m_config = m_configs[current_m_config][scanned_symbol]["final-m"]
+
+    next_tape_pointer = tape_pointer
+    next_tape = tape
+    print("tp: ", tape_pointer, "op: ", operations)
+    for op in operations:
+        if op[0] == "R":
+            next_tape_pointer += 1
+        elif op[0] == "L": 
+            next_tape_pointer -= 1
+        elif op[0] == "E":
+            next_tape = next_tape[:next_tape_pointer] + " " + next_tape[next_tape_pointer+1:]
+        elif op[0] == "P":
+            next_tape = next_tape[:next_tape_pointer] + op[1] + next_tape[next_tape_pointer+1:]
+
+
+    return (final_m_config, next_tape_pointer, next_tape)
+
+def calculate(m_configs, initial_m_config, tape):
+    m_config = initial_m_config
+    tape_pointer=0
+    while True:
+        print(m_config)
+        m_config, tape_pointer, tape = move(m_config, m_configs, tape_pointer, tape)
+    print(tape)
+
+if __name__ == "__main__":
+    m_configs = {"b" : {"None"  :{"operations":["P0"],"final-m":"b"},
+                        "0"     :{"operations":["R","R","P1"],"final-m":"b"},
+                        "1"     :{"operations":["R","R","P0"],"final-m":"b"}}}
+
+    m_configs = {   "b" : {"None":{"operations":["Pa","R","Pa","R","P0","R","R","P0","L","L"],"final-m":"o"},
+                            "1":{"operations":["Pa","R","Pa","R","P0","R","R","P0","L","L"],"final-m":"o"},
+                            "0":{"operations":["Pa","R","Pa","R","P0","R","R","P0","L","L"],"final-m":"o"}},
+
+                    "o" : {"1":{"operations":["R","Px","L","L","L"],"final-m":"o"},
+                            "0":{"operations":[],"final-m":"q"}},
+
+                    "q" : {"1":{"operations":["R","R"],"final-m":"q"},
+                            "0":{"operations":["R","R"],"final-m":"q"},
+                            "None":{"operations":["P1","L"],"final-m":"p"}},
+
+                    "p" : {"x":{"operations":["E","R"],"final-m":"q"},
+                            "a":{"operations":["R"],"final-m":"f"},
+                            "None":{"operations":["L","L"],"final-m":"p"}},
+
+                    "f" : {"None":{"operations":["P0","L","L"],"final-m":"o"},
+                            "1":{"operations":["R","R"],"final-m":"f"},
+                            "0":{"operations":["R","R"],"final-m":"f"}}
+                }
+    tape = " "*1000
+    calculate(m_configs, "b", tape)
